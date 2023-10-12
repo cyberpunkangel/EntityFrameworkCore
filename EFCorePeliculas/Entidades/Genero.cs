e9262762208879300c2154d7d01ba3c14cdb26dd
Angel López <149angel@hotmail.com>
2023-10-12 00:51:19 -0600

    Primer commit

---
+++ EFCorePeliculas/Entidades/Genero.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCorePeliculas.Entidades
+{
+    //[Index(nameof(Nombre), IsUnique = true)]
+    public class Genero : EntidadAuditable
+    {
+        public int Identificador { get; set; }
+        [ConcurrencyCheck]
+        public string Nombre { get; set; }
+        public bool EstaBorrado { get; set; }
+        public HashSet<Pelicula> Peliculas { get; set; }
+        //public string Ejemplo { get; set; }
+        //public string Ejemplo2 { get; set; }
+    }
+}
